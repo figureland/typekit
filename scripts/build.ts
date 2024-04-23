9f287f048e55f1a648edf04e795ae89d80da39ac
Toby Milner-Gulland <toby@figure.land>
2024-04-23 21:51:30 +0100

    feat: added entrypoints

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,7 +1,16 @@
 import dts from 'bun-plugin-dts'
 
 await Bun.build({
-  entrypoints: ['./src/index.ts'],
+  entrypoints: [
+    './src/index.ts',
+    './src/device.ts',
+    './src/guards.ts',
+    './src/map.ts',
+    './src/merge.ts',
+    './src/object.ts',
+    './src/promise.ts',
+    './src/time.ts'
+  ],
   outdir: './dist',
   minify: false,
   plugins: [dts()]
