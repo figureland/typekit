1c412d24b16c33e35e56dc8fec7b25ee6aaaca36
Toby Milner-Gulland <toby@figure.land>
2024-05-25 10:25:46 +0100

    feat: update API for promises

--- src/index.ts
+++ src/index.ts
@@ -46,5 +46,5 @@ export {
 export { sortMapToArray, NiceMap } from './map'
 export { getTimeSince } from './time'
 export { type Merge, simpleMerge } from './merge'
-export { isFulfilled, promiseSome, pickFulfilled } from './promise'
+export { isFulfilled, isRejected, settle, parseSettled } from './promise'
 export { shallowEquals, simpleEquals, type Equals } from './equals'
